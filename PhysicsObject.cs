a872fb71ac9269f028cadbd3a9acc0c535221bd0
Davie-Jones <yoikbendavie@hotmail.com>
2019-03-13 11:47:56 +0100

    2d game test

---
+++ PhysicsObject.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+
+    public float gravityModifier = 1f;
+    public float minGroundNormalY = 1f;
+
+    //maak variabelen om te kijken of iets de grond raakt en waar de grond is.
+    protected bool grounded;
+
+    //groundNormal = lijn die loodrecht op de grond staat
+    protected Vector2 groundNormal;
+
+    //protected = inherit en accessible maar niet aanpasbaar.
+    protected Vector2 velocity;
+
+
+    protected Vector2 targetVelocity;
+
+    //Rigidbody2d is object type die een unity object blootstelt aan de physics.
+    protected Rigidbody2D rb2d;
+    protected const float minMoveDistance = 0.001f;
+
+    //contactfilter kijkt of er een 2d collision is met iets anders dat een 2d collission heeft
+    protected ContactFilter2D contactFilter;
+
+    //maak een array van dingen die mogelijk een collission kunnen hebben voor de contactfilter
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+
+
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+    //maak een kleine forcefield om je karakter om te zorgen dat het nooit in een andere collissiondetector komt zodat het vast blijft zitten.
+    protected const float shellRadius = 0.01f;
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //don't use the standard triggers
+        contactFilter.useTriggers = false;
+
+        // gebruik de contactfilter om te checken of de LayerCollissionMask van het gameObject een collission ziet met een ander object
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        //gebruik de layermasker om dit te doen
+        contactFilter.useLayerMask = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity = Vector2.zero;
+        computeVelocity();
+        
+    }
+
+    protected virtual void computeVelocity()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime * 1.5f;
+
+        velocity.x = targetVelocity.x;
+
+        grounded = false;
+
+        //maak een nieuwe vector op dezelfde hoogte als de grondlijn, met een lijn die niet loodrecht op de grond staat maar over de grond heen loopt 
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+
+        //zwaartekracht
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 move = moveAlongGround * deltaPosition.x;
+
+        Movement(move, false);
+
+         move = Vector2.up * deltaPosition.y;
+
+        Movement(move, true);
+
+    }
+
+    void Movement(Vector2 move, bool yMovement)
+    {
+        //kijk hoe groot de lengte/kracht van een sprong is
+        float distance = move.magnitude;
+
+        //als kracht groter is dan minimum afstand
+        if (distance > minMoveDistance)
+        {
+            //kijk wat er in de rigid body 2d gebeurd, hoe beweegt het, is het ergens mee in contact, raakt het iets, wat is de magnitude en collision buffer
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+
+            //gooi alles wat rb2d raakt in een buffer
+            for (int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            //voor elk ding in de buffer, check of het contact maakt in de juiste hoek, zo ja: poppetje staat op grond en staat stil.
+            for (int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    grounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+
+
+        }
+
+        rb2d.position = rb2d.position + move.normalized * distance;
+    }
+}
---
+++ playerControler.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : PhysicsObject
+{
+
+    public float jumpSpeed = 7;
+    public float maxSpeed = 7;
+    public float yMovement = 0;
+    public float whenFalling = 10;
+    public bool falling = false;
+    public bool jumping = false;
+
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    protected override void computeVelocity()
+    {
+
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+
+        //roep de functie aan om de snelheden in te stellen
+        moveX(move);
+        moveY();
+        flipAnimation(move);
+
+        //spring als iemand op spatie drukt
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpSpeed * 2f;
+
+            if (Input.GetButtonUp("Jump"))
+            {
+                if (velocity.y > 0)
+                {
+                    velocity.y = velocity.y * .6f;
+                    Debug.Log("cancel de sprong");
+                }
+            }
+
+        }
+        targetVelocity = move * maxSpeed;
+    }
+
+
+    //functie om de velocityX te setten
+    private void moveX(Vector2 move)
+    {
+        if (move.x < -0.01f) //loop naar links
+        {
+            animator.SetFloat("velocityX", move.x * -1f);
+        }
+
+        if (move.x > 0.01f) //loop naar rechts
+        {
+            animator.SetFloat("velocityX", move.x);
+        }
+
+        if (move.x == 0) //loop niet
+        {
+            animator.SetFloat("velocityX", move.x);
+        }
+    }
+
+    //functie om spring en val animatie te tonen
+    private void moveY()
+    {
+
+        if (velocity.y > 0.01f || velocity.y < -0.01f) // als er beweging is op Y-as
+        {
+            grounded = false; //sta je niet op de grond
+            
+            if (yMovement < whenFalling && falling == false) //als de Y positie van het vorige frame hoger is dan de vorige, val je
+            {
+                Debug.Log("falling: ymovement =" + yMovement);
+                Debug.Log("falling: velocity.y =" + velocity.y);
+                falling = true;
+                jumping = false;
+                animator.SetBool("playerFalling", falling);
+                animator.SetBool("playerJumping", jumping);
+                Debug.Log("falling");
+            }
+
+            else if (yMovement > whenFalling && jumping == false)
+            { //als je niet valt en er wel Y beweging is ga je omhoog en dus spring je
+                falling = false;
+                jumping = true;
+                animator.SetBool("playerFalling", falling);
+                animator.SetBool("playerJumping", jumping);
+                Debug.Log("jumping: ymovement = " + yMovement);
+                Debug.Log("jumping: velocity.y = " + velocity.y);
+            }
+        }
+
+        else if (velocity.y == 0f) //als er geen Y beweging is dan sta je op de grond
+        {
+            grounded = true;
+            falling = false;
+            jumping = false;
+            animator.SetBool("playerFalling", falling);
+            animator.SetBool("playerJumping", jumping);
+            Debug.Log("grounded");
+        }
+
+        yMovement = velocity.y; //update de vorige y positie met de huidige y positie
+
+    }
+
+
+    //flip animatie als player andere kant op rent
+    private void flipAnimation(Vector2 move)
+    {
+        if (move.x < -0.01f && spriteRenderer.flipX == false)
+        {
+            spriteRenderer.flipX = true;
+
+        }
+
+        if (move.x > 0.01f && spriteRenderer.flipX == true)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
